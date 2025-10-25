2446b1a3d9e3d328fa31c40dd028f19ac0f41752
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-10-25 19:29:48 +0100

    add modeling/entities docs

--- mdx-components.js
+++ mdx-components.js
@@ -1,4 +1,5 @@
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
+import * as LucideIcons from 'lucide-react'
  
 // Get the default MDX components
 const themeComponents = getThemeComponents()
@@ -7,6 +8,7 @@ const themeComponents = getThemeComponents()
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
-    ...components
+    ...components,
+    ...LucideIcons
   }
 }
