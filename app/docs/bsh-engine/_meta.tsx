59adae6d426b1ed69c08421ac7176dbdaaf37d7c
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-10-26 19:55:43 +0100

    finish email api documentation

--- app/docs/bsh-engine/_meta.tsx
+++ app/docs/bsh-engine/_meta.tsx
@@ -6,7 +6,7 @@ export default {
   'security': 'Security',
   'triggers': 'Triggers',
   'files': 'Files',
-  'emails': 'Emails',
+  'mailing': 'Mailing',
   'caching': <Title title='Caching' badge='planned' />,
   'websocket': <Title title='WebSocket' badge='planned' />,
   'packaging': <Title title='Packaging' badge='planned' />,
---
+++ app/docs/bsh-engine/api/mailing/_meta.tsx
@@ -0,0 +1,6 @@
+import type { MetaRecord } from 'nextra';
+import { EndpointTitle } from '../../../../../components';
+
+export default {
+  'send-email': <EndpointTitle method='POST' title='Send Email'/>
+} satisfies MetaRecord;
