0a991da2bcd4d92b276b89a30e3259f49cf0c0e8
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-10-26 16:16:59 +0100

    add Import Entities as planned

--- app/docs/bsh-engine/api/entities/_meta.tsx
+++ app/docs/bsh-engine/api/entities/_meta.tsx
@@ -7,8 +7,9 @@ export default {
   'update-entity': <EndpointTitle method='PUT' title='Update'/>,
   'delete-entity': <EndpointTitle method='DELETE' title='Delete'/>,
   'search-entities': <EndpointTitle method='POST' title='Search'/>,
-  'export-entities': <EndpointTitle method='POST' title='Export'/>,
   'batch-insert-entities': <EndpointTitle method='POST' title='Batch Insert'/>,
   'batch-update-entities': <EndpointTitle method='PUT' title='Batch Update'/>,
   'batch-delete-entities': <EndpointTitle method='POST' title='Batch Delete'/>,
+  'export-entities': <EndpointTitle method='POST' title='Export'/>,
+  'import-entities': <EndpointTitle method='POST' title='Import' badge='planned'/>,
 } satisfies MetaRecord;
