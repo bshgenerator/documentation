c8efd823231effe5de66b8ff3b0cf21efa3a315a
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-10-26 20:08:22 +0100

    Add Postman links API documentation

---
+++ tech-icons/index.ts
@@ -0,0 +1,6 @@
+import PostmanIcon from './PostmanIcon';
+import TypescriptIcon from './TypescriptIcon';
+import DockerIcon from './DockerIcon';
+import GithubIcon from './GithubIcon';
+
+export { PostmanIcon, TypescriptIcon, DockerIcon, GithubIcon };
