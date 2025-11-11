304ec9901b1c125587f95b9b08a88fcc12ecb24b
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-11-11 20:41:07 +0100

    Add new blog post "Mastering Java 21"

--- app/blogs/_meta.ts
+++ app/blogs/_meta.ts
@@ -1,7 +1,10 @@
 import type { MetaRecord } from "nextra";
 
 export default {
+  "mastering-java-21": {
+    title: "Mastering Java 21"
+  },
   "how-to-use-bshg-validation-lib": {
     title: "How to Use @bshg/validation"
-  }
+  },
 } satisfies MetaRecord;
