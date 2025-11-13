b7813db595b1adddb739af6ab3400ace759caa3e
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-11-14 00:52:44 +0100

    Add 'Search Entities by Query' endpoint documentation

--- app/docs/bsh-engine/api/entities/_meta.tsx
+++ app/docs/bsh-engine/api/entities/_meta.tsx
@@ -7,6 +7,7 @@ export default {
   'update-entity': <EndpointTitle method='PUT' title='Update'/>,
   'delete-entity': <EndpointTitle method='DELETE' title='Delete'/>,
   'search-entities': <EndpointTitle method='POST' title='Search'/>,
+  'search-entities-by-query': <EndpointTitle method='GET' title='Search by Query' badge='new'/>,
   'insert-entities-batch': <EndpointTitle method='POST' title='Batch Insert'/>,
   'update-entities-batch': <EndpointTitle method='PUT' title='Batch Update'/>,
   'delete-entities-batch': <EndpointTitle method='POST' title='Batch Delete'/>,
