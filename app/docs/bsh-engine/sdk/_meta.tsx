1f29c99da48914df34b8aa2cb0fd965566b39d97
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-12-04 20:43:27 +0100

    feat: update BSH Engine SDK documentation

--- app/docs/bsh-engine/sdk/_meta.tsx
+++ app/docs/bsh-engine/sdk/_meta.tsx
@@ -1,16 +1,10 @@
 import type { MetaRecord } from 'nextra';
-import { Title } from '../../../../components/Title';
 
 export default {
-  'typescript': {
-    title: <Title title='Typescript' badge='new' />
-  },
-  'java': {
-    title: 'Java',
-    display: 'hidden',
-  },
-  'python': {
-    title: 'Python',
-    display: 'hidden',
-  },
+    'quick-start': 'Quick Start',
+    'configuration': 'Configuration',
+    'custom-http-client': 'Custom HTTP Client',
+    'authentication': 'Authentication',
+    'services': 'Services',
+    'interceptors': 'Interceptors',
 } satisfies MetaRecord;
