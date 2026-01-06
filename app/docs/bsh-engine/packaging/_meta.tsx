9f356578bb6874fcd777cd95ba085496257a5570
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2026-01-07 00:15:21 +0100

    add plugin docs and cli docs

---
+++ app/docs/bsh-engine/packaging/_meta.tsx
@@ -0,0 +1,6 @@
+import type { MetaRecord } from 'nextra';
+import { Title } from '../../../../components';
+
+export default {
+    'index': <Title title='Packaging' badge='planned' />
+} satisfies MetaRecord;
---
+++ app/docs/bsh-engine/packaging/plugins/_meta.tsx
@@ -0,0 +1,10 @@
+import type { MetaRecord } from 'nextra';
+import { Title } from '../../../../../components';
+
+export default {
+    // 'index': <Title title='Plugins' badge='new' />,
+    'config-file': <Title title='Config File' />,
+    'manifest-files': <Title title='Manifest Files' />,
+    'content-files': <Title title='Content Files' />,
+} satisfies MetaRecord;
+
